bc25b92a8fd93df817a0d81539de4f70a9c2dff1
THorne93 <taylorhorne1993@gmail.com>
2024-02-21 22:39:25 +0100

    made changes to chipadvisor. Should be 1 page with foreach that functions to open close content and then another page that is a SPA work in progress

--- dist/chipadvisor.js
+++ dist/chipadvisor.js
@@ -1,51 +1,74 @@
 window.addEventListener("load", (event) => {
-    fetch('chip-data1.json')
-        .then((response) => response.json())
-        .then((json) => json.forEach((item, i) => {
-        setTimeout(() => {
-            const card = document.createElement("div");
-            card.classList.add("square");
-            const app = document.getElementById("app");
-            app.appendChild(card);
-            const icon = document.createElement("img");
-            icon.src = item.icon;
-            card.appendChild(icon);
-            icon.classList.add("pic");
-            const title = document.createElement("h1");
-            title.classList.add("h1");
-            title.innerHTML = item.location[0].name;
-            card.appendChild(title);
-            const url = document.createElement("a");
-            url.href = item.url;
-            card.appendChild(url);
-            card.addEventListener('click', (e) => {
-                open(url.href, "_self");
-            });
-        }, i * 100);
-    }));
-    const ChooseCompanyCards = [
-        {
-            title: 'Nandos',
-            url: 'https://chipsreviewforu.wordpress.com/2022/02/10/chip-review-56-kanival-burger-malaga-spain/',
-            icon: './img/chipadvisor/nandos.jpg',
-            href: 'tbc'
-        },
-        {
-            title: 'Donner Haus',
-            url: 'tbc',
-            icon: './img/chipadvisor/donner haus.jpeg',
-        },
-        {
-            title: 'Titchfield Mill',
-            url: 'tbc',
-            icon: './img/chipadvisor/titchfield.png',
-        },
-        {
-            title: 'Kanival Burger',
-            url: 'https://chipsreviewforu.wordpress.com/2022/02/10/chip-review-56-kanival-burger-malaga-spain/',
-            icon: './img/chipadvisor/kanival.jpeg',
-        },
-    ];
+    var x = 100;
+    const main = document.getElementById("main");
+    const contentPane = document.getElementById("contentPane");
+    function getChips() {
+        fetch('chip-data1.json')
+            .then((response) => response.json())
+            .then((json) => json.forEach((item, i) => {
+            setTimeout(() => {
+                const li = document.createElement("li");
+                li.classList.add("card");
+                const container = document.getElementById("card-container");
+                container.appendChild(li);
+                const card = document.createElement("div");
+                card.classList.add("square");
+                li.appendChild(card);
+                const icon = document.createElement("img");
+                icon.src = item.icon;
+                card.appendChild(icon);
+                icon.classList.add("pic");
+                const title = document.createElement("h1");
+                title.classList.add("h1");
+                title.innerHTML = item.location[0].name;
+                card.appendChild(title);
+                const url = document.createElement("a");
+                url.href = item.url;
+                card.appendChild(url);
+                card.addEventListener('click', (e) => {
+                    contentPane.innerHTML = '';
+                    const text = document.createElement("p");
+                    text.innerHTML = item.content;
+                    const location = item.location[0];
+                    centerMap(parseFloat(location.lat), parseFloat(location.long));
+                    contentPane.appendChild(text);
+                });
+            }, i * x);
+        }));
+    }
+    function centerMap(lat, lng) {
+        if (map instanceof google.maps.Map) {
+            map.panTo({ lat: lat, lng: lng });
+            map.setZoom(15);
+        }
+        else {
+            console.error('map is not an instance of google.maps.Map');
+        }
+    }
+    getChips();
+    if (x == 100) {
+        x = 10;
+    }
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.addEventListener('mousemove', function (event) {
+        const containerRect = cardContainer.getBoundingClientRect();
+        const mouseX = event.clientX - containerRect.left;
+        const containerWidth = cardContainer.offsetWidth;
+        if (mouseX < 60) { // Adjust this value as needed
+            if (mouseX < 30) {
+                cardContainer.scrollLeft -= 20; // Scroll left
+            }
+            else
+                cardContainer.scrollLeft -= 10;
+        }
+        else if (mouseX > containerWidth - 60) { // Adjust this value as needed
+            if (mouseX > containerWidth - 30) {
+                cardContainer.scrollLeft += 20;
+            }
+            else
+                cardContainer.scrollLeft += 10; // Scroll right
+        }
+    });
     // url.innerHTML = item.title
     // item.title.appendChild(url)
     // href = item.url
